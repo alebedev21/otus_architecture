99e34bf4275bae65962c4546700ed0627df18970
lebedev <a.lebedev@mvsystem.ru>
2024-08-09 15:41:32 +0300

    done
    (cherry picked from commit d2001221628154c39659916cbf384b13953aef25)

---
+++ Server/Entities/UnchangeablePositionObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace Server.Entities;
+
+#pragma warning disable CS0649 // Field is never assigned to, and will always have its default value
+public class UnchangeablePositionObject
+{
+    private Vector2 _position;
+    private Vector2 _velocity;
+
+    public Vector2 GetPosition()
+    {
+        return _position;
+    }
+
+    public Vector2 GetVelocity()
+    {
+        return _velocity;
+    }
+
+    public void SetVelocity(Vector2 velocity)
+    {
+        _velocity = velocity;
+    }
+}
